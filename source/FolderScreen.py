7b75ab7c606fc81623cd6850b5d1eb19b8b9fb55
The5CentPiece <john.a.nichols00@gmail.com>
2018-08-11 21:45:18 -0700

    click the folder for fun

--- source/FolderScreen.py
+++ source/FolderScreen.py
@@ -1,16 +1,19 @@
-import pygame as pg, sys, math, time, os
+import pygame as pg, sys, math, time, os, ctypes
 class Folder:
     def __init__(self):
-        self.width = 1920
-        self.height = 1080
+        ctypes.windll.user32.SetProcessDPIAware()
+        self.width = ctypes.windll.user32.GetSystemMetrics(0)
+        self.height = ctypes.windll.user32.GetSystemMetrics(0)
         self.background = pg.image.load("assets/background.png")
         self.folderOpen = pg.image.load("assets/FileFolderOpen.png")
         self.folderOpen = pg.transform.scale(self.folderOpen,(1720,880))
-        self.startscreen = pg.display.set_mode((self.width, self.height),pg.NOFRAME)
+        self.screen_color = (128, 0, 0)
         self.clock = pg.time.Clock()
     def runScreen(self):
         '''Starts the folder screen'''
+        self.startscreen = pg.display.set_mode((self.width, self.height),pg.NOFRAME)
         pg.init()
+        self.startscreen.fill(self.screen_color)
         self.startscreen.blit(self.background, (0,0))
         self.startscreen.blit(self.folderOpen, (100,100))
         pg.display.update()
